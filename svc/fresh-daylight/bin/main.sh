4096e079a4d27bbd68642a8854fcac7dd58c7e01
Chris Lalos <chris.lalos@gmail.com>
2023-04-25 23:05:13 -0500

    fixes

--- svc/fresh-daylight/bin/main.sh
+++ svc/fresh-daylight/bin/main.sh
@@ -1,6 +1,5 @@
-/bin/sh
+#! /bin/sh
 printf '%s\n' "Installing daylight ..."
-printf '%s\n' 
 url=https://raw.githubusercontent.com/daylight-public/daylight/sentience/daylight.sh
 curl --silent --remote-name --output-dir /opt/bin "$url"
 printf '%s\n' Done.
