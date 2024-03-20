b36508b5df04a278412f97683badd1f6c8e57df0
Chris Lalos <chris.lalos@gmail.com>
2024-03-20 00:28:34 -0500

    enhanced install-fresh-daylight-svc.sh

--- install-fresh-daylight-svc.sh
+++ install-fresh-daylight-svc.sh
@@ -3,3 +3,4 @@ sudo mkdir -p /opt/bin/
 sudo chown -R ubuntu:ubuntu /opt/bin/
 curl --silent --remote-name --output-dir /opt/bin/ "$url"
 chmod 777 /opt/bin/daylight.sh
+/opt/bin/daylight.sh install-fresh-daylight-svc
