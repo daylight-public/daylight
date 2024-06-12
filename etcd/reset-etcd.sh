4fe939fa5180eb05de4bdc69727eff5b4b67ff83
Chris Lalos <chris.lalos@gmail.com>
2024-06-12 08:35:49 -0500

    added replace-nginx

--- etcd/reset-etcd.sh
+++
@@ -1,5 +0,0 @@
-dir=/var/lib/etcd/
-
-sudo rm -r "$dir" 2>/dev/null
-sudo mkdir -p "$dir"
-sudo chown -R ubuntu:ubuntu "$dir"
--- etcd/uninstall-etcd.sh
+++
@@ -1,5 +0,0 @@
-data_dir=/var/lib/etcd/
-install_dir=/opt/etcd/
-
-sudo rm -r "$data_dir" 2>/dev/null
-sudo rm -r "$install_dir" 2>/dev/null
