c7ad33968ba33a31d799933b7746598a6864f522
Chris Lalos <chris.lalos@gmail.com>
2024-10-10 06:16:01 -0500

    Update HowTo-download-daylight.sh

--- HowTo-download-daylight.sh
+++ HowTo-download-daylight.sh
@@ -3,6 +3,13 @@ To download `daylight.sh` to `/opt/bin` ...
 curl --silent --remote-name --output-dir /opt/bin https://raw.githubusercontent.com/daylight-public/daylight/main/daylight.sh
 ```
 
+To create the `daylight.sh` download folder, download `daylight.sh`, and install the `fresh-daylight` service to download updates ...
+```
+sudo mkdir -p /opt/bin/
+sudo chown -R ubuntu:ubuntu /opt/bin/
+curl --remote-name --output-dir /opt/bin https://raw.githubusercontent.com/daylight-public/daylight/main/daylight.sh
+```
+
 To download and run the script like a maniac ...
 ```
 curl --silent /opt/bin/daylight.sh https://raw.githubusercontent.com/daylight-public/daylight/sentience/daylight.sh | bash
