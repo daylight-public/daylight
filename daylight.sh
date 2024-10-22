c16db2aadf86b89592492c5bc81e6d8d797db716
Chris Lalos <chris.lalos@gmail.com>
2024-10-22 16:59:44 -0500

    fixes to watch-daylight run.sh

--- daylight.sh
+++ daylight.sh
@@ -2417,20 +2417,28 @@ update-and-restart ()
 # Generate a run script for the watch-daylight.service
 watch-daylight-gen-run-script ()
 {
-    cat <<- 'EOT'
-	#! /usr/bin/env bash
-
+    cat <<- "EOT"
 	main () 
 	{ 
-        printf "Downloading current script ...";
-        /opt/etcd/etcdctl \
-            --discovery-srv hello.dylt.dev \
-            get --print-value-only daylight.sh >/opt/bin/daylight.sh || return
-        printf 'Watching for further updates ....'
+	    printf "Downloading current script ...\n";
+	    # etcdctl get, redirect to tempfile, and cp to proper location on success
+	    local tmpfile; tmpfile="$(mktemp --tmpdir daylight.sh.XXXXXX)" || return
+	    if ! /opt/etcd/etcdctl \
+	        --discovery-srv hello.dylt.dev \
+	        get --print-value-only daylight.sh >"$tmpfile";
+	    then
+	        local rc=$?
+	        printf '%s\n' "Failed to download daylight.sh from cluster" >&2
+	        return $?
+	    else
+	        printf 'Download succeeded. Copying script to final location ...\n'
+	        cp "$tmpfile" /opt/bin/daylight.sh
+	    fi
+	    printf 'Watching for further updates ....'
 	    /opt/etcd/etcdctl \
 	        --discovery-srv hello.dylt.dev \
 	        watch daylight.sh \
-	            -- sh -c '{ printf "Downloading update ..."; /opt/etcd/etcdctl --discovery-srv hello.dylt.dev get --print-value-only daylight.sh >/opt/bin/daylight.sh; printf "Complete.\n"; }' || return
+	            -- sh -c '{ printf "Downloading update ..."; tmpfile="$(mktemp --tmpdir daylight.sh.XXXXXX)"; /opt/etcd/etcdctl --discovery-srv hello.dylt.dev get --print-value-only daylight.sh >"$tmpfile"; cp "$tmpfile" /opt/bin/daylight.sh; printf "Complete.\n"; }' || return
 	}
 
 	main "$@"
@@ -2466,7 +2474,6 @@ watch-daylight-install-service ()
     chmod 755 "$svcFolder/run.sh"
     sudo systemctl enable "$svcFolder/$svc.service"
     sudo systemctl start "$svc"
-
 }
 
 
